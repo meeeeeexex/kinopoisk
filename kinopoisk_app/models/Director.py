3bc829c2dbf9de5a476109abbedf24ea462b4360
Vlad <was2wass@gmail.com>
2022-05-24 21:57:20 +0300

    Added some features

--- kinopoisk_app/models/Director.py
+++ kinopoisk_app/models/Director.py
@@ -5,6 +5,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Director(Person):
+    filmography = models.ManyToManyField('Movie', null=True, blank=True)
+
     class Meta:
         verbose_name = "Режиссер"
         verbose_name_plural = "Режиссеры"
