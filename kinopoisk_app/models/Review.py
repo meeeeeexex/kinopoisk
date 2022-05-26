f2766d00300a03a21ef4c44f51e87d672b0cb161
Ваше Имя <you@example.com>
2022-05-26 21:00:18 +0300

    Fixed some bugs

--- kinopoisk_app/models/Review.py
+++ kinopoisk_app/models/Review.py
@@ -2,11 +2,9 @@ from django.contrib.auth.models import User
 from django.db import models
 import uuid
 
-from kinopoisk_app.models.common import BaseModel
 
-
-class Review(BaseModel):
-    # id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+class Review(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey('kinopoisk_app.Movie', on_delete=models.CASCADE)
     review_text = models.TextField(null=False)
