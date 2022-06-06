9e0603d1d0d4e07755e67289e1e1bb03497adde8
Vlad <was2wass@gmail.com>
2022-06-06 22:31:16 +0200

    actors slug

--- kinopoisk_app/models/Actor.py
+++ kinopoisk_app/models/Actor.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 from kinopoisk_app.models.common.Person import Person
 
 
 class Actor(Person, models.Model):
+    slug = models.SlugField(null=True, unique=True)
+
+    def get_absolute_url(self):
+        return reverse("actor_detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            print(self.first_name + ' ' + self.last_name)
+            self.slug = slugify(self.first_name + ' ' + self.last_name)
+        return super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = "Актер"
@@ -11,3 +23,5 @@ class Actor(Person, models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+ # 0f096c78-72f3-415f-9417-e6b0c4b8c55f | Frank          | Darabont            |     | Not specified | frank-darabont
