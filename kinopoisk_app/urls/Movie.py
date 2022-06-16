8adbb03af82f7dd4bfdd6e8d468b2214a9d0a872
Ilya <ilyakhomutov613@gmail.com>
2022-06-16 21:58:32 +0300

    Added top films

--- kinopoisk_app/urls/Movie.py
+++ kinopoisk_app/urls/Movie.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from kinopoisk_app.views.Movie import MovieView
+from kinopoisk_app.views.Movie import MovieView, MovieTOPView
 
 urlpatterns = [
     path('movies/', MovieView.as_view({'get': 'list'})),
     path('movie/<slug:slug>/', MovieView.as_view({'get': 'retrieve'}), name='movie_detail'),
+    path('movies/top100', MovieTOPView.as_view({'get': 'list'})),
 ]
